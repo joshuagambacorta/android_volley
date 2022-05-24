5c1bc75c911aea13d88e34355b726494cf16048c
joshua <joshua.gambacorta@gmail.com>
2022-05-24 11:33:50 +0200

    feat: creation two activity and check listener for login.

--- app/src/main/java/com/example/androidvolley/MainActivity.java
+++ app/src/main/java/com/example/androidvolley/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.androidvolley;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
+    
+    Button check_login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        check_login = (Button)findViewById(R.id.check_login);
+
+        check_login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "login", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
